1687700ce87250f020460caa39da04ebc55fd698
m19e <nexus.me7290@gmail.com>
2020-01-27 21:11:31 +0900

    ファイル読み込みできた

--- command/read.go
+++ command/read.go
@@ -2,11 +2,36 @@ package command
 
 import (
 	"fmt"
+	"os"
+	"io/ioutil"
 	"github.com/urfave/cli"
 )
 
 func CmdRead(c *cli.Context) error {
 	// Write your code here
-	fmt.Println("Hello friend!")
+	
+	// p, _ := os.Getwd()
+	// fmt.Println(p)
+
+	// ls, _ := ioutil.ReadDir("./")
+	// for _, file := range ls {
+	// 	// fmt.Println(file.Name())
+	// }
+
+	fmt.Println("read file...")
+	// open file
+	f, err := os.Open(c.Args().Get(0))
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	defer fmt.Println("")
+
+	// read all text
+	b, _ := ioutil.ReadAll(f)
+
+	// output
+	fmt.Println(string(b))
+
 	return nil
 }
