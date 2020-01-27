5a08b51179c2095d32874ea45ab998195c383586
m19e <nexus.me7290@gmail.com>
2020-01-27 18:50:10 +0900

    とりあえずCommands内に書けば動きそう

--- command/read.go
+++ command/read.go
@@ -1,9 +1,12 @@
 package command
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"github.com/urfave/cli"
+)
 
 func CmdRead(c *cli.Context) error {
 	// Write your code here
-
+	fmt.Println("Hello friend!")
 	return nil
 }
