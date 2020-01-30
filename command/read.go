66a8f56160720f0bbf269df9f741ae57fbf44fb0
m19e <nexus.me7290@gmail.com>
2020-01-30 15:06:28 +0900

    漢字判定が(一応)できるようになった！

--- command/read.go
+++ command/read.go
@@ -5,7 +5,13 @@ import (
 	"os"
 	"io/ioutil"
 	"github.com/urfave/cli"
+	"regexp"
 )
+
+func isKanji(r rune) bool {
+	reg := regexp.MustCompile(`[\x{3400}-\x{9FFF}]`)
+	return reg.MatchString(string(r))
+}
 
 func CmdRead(c *cli.Context) error {
 	// Write your code here
@@ -18,20 +24,30 @@ func CmdRead(c *cli.Context) error {
 	// 	// fmt.Println(file.Name())
 	// }
 
-	fmt.Println("read file...")
+	fmt.Println(">> read file...")
 	// open file
 	f, err := os.Open(c.Args().Get(0))
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	defer fmt.Println(">> finish")
 	defer f.Close()
-	defer fmt.Println("")
 
 	// read all text
 	b, _ := ioutil.ReadAll(f)
 
 	// output
-	fmt.Println(string(b))
+	// fmt.Println(string(b))
 
+	for _, char := range string(b) {
+		if isKanji(char) {
+			fmt.Printf("%s", string(char))
+		} else {
+			//fmt.Printf("%s", string(char))
+		}
+	}
+
+	fmt.Println("")
 	return nil
 }
